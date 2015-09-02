ebd6a180edb1b7a57aa2109c6d4db09a607a0971
markbernard <captainjester@hotmail.com>
2015-09-02 10:30:40 -0400

    Final game.

--- invaders/invaders.js
+++ invaders/invaders.js
@@ -16,6 +16,8 @@ var Invaders = {
     playerBulletY: 0,
     playerBulletFired: false,
     playerBase: null,
+    playerBaseGrid: null,
+    playerSpeed: 3,
     left: false,
     right: false,
     resourceCounter: 0,
@@ -27,6 +29,7 @@ var Invaders = {
     alienY: 0,
     alienDeltaX: 0,
     alienDeltaY: 0,
+    alienBottomRow: 4,
     alienCount: 55,
     alienBullet: null,
     alien: [[]],
@@ -40,8 +43,11 @@ var Invaders = {
     alienMotherShipSpeed: 0,
     alienMotherShipCounter: 0,
     alienMotherShipOn: false,
+    alienMotherShipScore: 0,
+    alienMotherShipScoreCounter: 0,
     mouseX: 0,
-    mouseY: 0
+    mouseY: 0,
+    shotX: 0
 };
 
 $(function () {
@@ -52,6 +58,27 @@ $(function () {
     reset();
 });
 
+function lineIntersectsRectangle(x1, y1, x2, y2, minX, minY, maxX, maxY) {  
+    // Completely outside.
+    if ((x1 <= minX && x2 <= minX) || (y1 <= minY && y2 <= minY) || (x1 >= maxX && x2 >= maxX) || (y1 >= maxY && y2 >= maxY))
+        return false;
+
+    var m = (y2 - y1) / (x2 - x1);
+
+    var y = m * (minX - x1) + y1;
+    if (y > minY && y < maxY) return true;
+
+    y = m * (maxX - x1) + y1;
+    if (y > minY && y < maxY) return true;
+
+    var x = (minY - y1) / m + x1;
+    if (x > minX && x < maxX) return true;
+
+    x = (maxY - y1) / m + x1;
+    if (x > minX && x < maxX) return true;
+
+    return false;
+}
 function reset() {
     "strict mode";
     var i;
@@ -80,12 +107,30 @@ function reset() {
     }
     Invaders.playerBullet.src = "bullet.png";
     
-    Invaders.playerBase = new Image;
-    Invaders.resourceCounter += 1;
-    Invaders.playerBase.onload = function () {
+    Invaders.playerBase = [new Image, new Image, new Image];
+    Invaders.resourceCounter += 3;
+    Invaders.playerBase[0].onload = function () {
         Invaders.resourceCounter -= 1;
     }
-    Invaders.playerBase.src = "base.png";
+    Invaders.playerBase[0].src = "base.png";
+    Invaders.playerBase[1].onload = function () {
+        Invaders.resourceCounter -= 1;
+    }
+    Invaders.playerBase[1].src = "baseleft.png";
+    Invaders.playerBase[2].onload = function () {
+        Invaders.resourceCounter -= 1;
+    }
+    Invaders.playerBase[2].src = "baseright.png";
+    
+    //set up player base configuration
+    Invaders.playerBaseGrid = new Array;
+    for (i=0;i<6;i += 1) {
+        Invaders.playerBaseGrid.push(
+            [[1, 0, 2],
+            [0, 0, 0],
+            [0, 0, 0],
+            [0, 3, 0]]);
+    }
     
     Invaders.alienShip = [new Image, new Image, new Image, new Image];
     Invaders.resourceCounter += 4;
@@ -156,6 +201,7 @@ function createAliens(speed) {
     Invaders.alienTop = 0;
     Invaders.alienX = 0;
     Invaders.alienY = 25;
+    Invaders.alienBottomRow = 4;
     Invaders.alienDeltaX = speed;
     Invaders.alienDeltaY = 10;
     Invaders.alienCount = 55;
@@ -167,25 +213,38 @@ function update() {
     "strict mode";
     var collisionX, collisionY, i, advanceColumn = true, shotX, shotY;
     if (Invaders.left === true) {
-        Invaders.playerShipX -= 2.5;
+        Invaders.playerShipX -= Invaders.playerSpeed;
         if (Invaders.playerShipX <= 0) {
             Invaders.playerShipX = 0;
         }
     } else if (Invaders.right === true) {
-        Invaders.playerShipX += 2.5;
+        Invaders.playerShipX += Invaders.playerSpeed;
         if (Invaders.playerShipX >= 768) {
             Invaders.playerShipX = 768;
         }
     }
     if (Invaders.playerBulletFired === true) {
-        if (Invaders.playerBulletY >= 514) {
-            for (x=0;x<6;x += 1) {
-                if (Invaders.playerBulletX >= x * 110 + 98 && Invaders.playerBulletX <= x * 110 + 147 &&
-                        Invaders.playerBulletY >= 514 && Invaders.playerBulletY <= 543) {
+        var shotColumn = (Invaders.playerBulletX - 101) / 110,
+            shotRow, 
+            baseColumn;
+        if (shotColumn >= 0 && shotColumn < 6) {
+            shotColumn = Math.floor(shotColumn);
+            baseColumn = Math.floor(((Invaders.playerBulletX - 101) - (shotColumn * 110)) / 16);
+            for (shotRow=0;shotRow<4;shotRow += 1) {
+                if (Invaders.playerBaseGrid[shotColumn][shotRow][baseColumn] !== 3 && 
+                        baseColumn >= 0 && baseColumn < 3 &&
+                        lineIntersectsRectangle(
+                            Invaders.playerBulletX, Invaders.playerBulletY, 
+                            Invaders.playerBulletX, Invaders.playerBulletY + 10, 
+                            (shotColumn * 110 + 101) + baseColumn * 16, 516 + (shotRow * 7), 
+                            (shotColumn * 110 + 117) + baseColumn * 16, 516 + (shotRow * 7) + 7)) {
                     Invaders.playerBulletFired = false;
+                    Invaders.playerBaseGrid[shotColumn][shotRow][baseColumn] = 3;
                 }
             }
         }
+    }
+    if (Invaders.playerBulletFired === true) {
         collisionX = Math.floor((Invaders.playerBulletX - Invaders.alienX) / 48);
         collisionY = Math.floor((Invaders.playerBulletY - Invaders.alienY) / 48);
         if (collisionX >= 0 && collisionX <= 10 && collisionY >= 0 && collisionY <= 4 
@@ -224,13 +283,24 @@ function update() {
             }
         }
         
+        if (Invaders.alienMotherShipScoreCounter > 0) {
+            Invaders.alienMotherShipScoreCounter -= 1;
+        }
         if (Invaders.playerBulletX >= Invaders.alienMotherShipX && 
                 Invaders.playerBulletX <= Invaders.alienMotherShipX + 48 &&
                 Invaders.playerBulletY >= Invaders.alienMotherShipY + 20 &&
                 Invaders.playerBulletY <= Invaders.alienMotherShipY + 34) {
             Invaders.alienMotherShipOn = false;
             Invaders.playerBulletFired = false;
-            Invaders.score += ((Math.floor(Math.random() * 9) + 2) * 50) + (Invaders.level * 100) + 100;
+            Invaders.alienMotherShipScore = ((Math.floor(Math.random() * 9) + 2) * 50) + (Invaders.level * 100) + 100;
+            Invaders.alienMotherShipScoreCounter = 30;
+            Invaders.score += Invaders.alienMotherShipScore;
+            if (Invaders.alienMotherShipX < 5) {
+                Invaders.alienMotherShipX = 5;
+            }
+            if (Invaders.alienMotherShipX > 747) {
+                Invaders.alienMotherShipX = 747;
+            }
         }
         
         Invaders.playerBulletY -= 10;
@@ -243,9 +313,14 @@ function update() {
         Invaders.alienDeltaX *= -1;
         Invaders.alienY += Invaders.alienDeltaY;
     }
+    if (Invaders.alienY + 48 + (Invaders.alienBottomRow * 48) >= 600) {
+        Invaders.lives = 0;
+        Invaders.alienDeltaX = 0;
+        Invaders.alienDeltaY = 0;
+    }
 
     if (Invaders.alienShot === false) {
-        Invaders.alienShotTimer -= 30;
+        Invaders.alienShotTimer -= 1;
     } else {
         Invaders.alienShotY += 5;
         if (Invaders.alienShotX >= (Invaders.playerShipX + 3) && Invaders.alienShotX <= (Invaders.playerShipX + 28) &&
@@ -253,31 +328,48 @@ function update() {
                 Invaders.lives > 0) {
             Invaders.alienShot = false;
             Invaders.lives -= 1;
-        }
-    }
-    if (Invaders.alienShotTimer <= 0) {
-        Invaders.alienShotTimer = (Math.random() * 3000);
-        shotX = Math.floor(Math.random() * 11);
-        shotY = Math.floor(Math.random() * 5);
-        while (Invaders.alien[shotX][shotY] === false) {
-            shotX += 1;
-            if (shotX > 10) {
-                shotX = 0;
-                shotY += 1;
-                if (shotY > 4) {
-                    shotY = 0;
+        } else {
+            var shotColumn = (Invaders.alienShotX - 101) / 110,
+                shotRow = (Invaders.alienShotY - 516) / 7, 
+                baseColumn;
+            if (shotRow >= 0 && shotRow < 4 && shotColumn >= 0 && shotColumn < 6) {
+                shotRow = Math.floor(shotRow);
+                shotColumn = Math.floor(shotColumn);
+                baseColumn = Math.floor(((Invaders.alienShotX - 101) - (shotColumn * 110)) / 16);
+                if (baseColumn >= 0 && baseColumn < 3 && Invaders.playerBaseGrid[shotColumn][shotRow][baseColumn] !== 3) {
+                    Invaders.alienShot = false;
+                    Invaders.playerBaseGrid[shotColumn][shotRow][baseColumn] = 3;
                 }
             }
         }
-        Invaders.alienShotX = shotX * 48 + Invaders.alienX + 20;
-        Invaders.alienShotY = shotY * 48 + Invaders.alienY + 36;
-        Invaders.alienShot = true;
+    }
+    if (Invaders.alienShotTimer <= 0) {
+        Invaders.alienShotTimer = (Math.random() * 100);
+        Invaders.shotX = Math.floor((Invaders.playerShipX - Invaders.alienX) / 48);
+        if (Invaders.shotX < 0 || Invaders.shotX > 10) {
+            Invaders.alienShotTimer = 10;
+        } else {
+            shotY = Math.floor(Math.random() * 5);
+            while (Invaders.alien[Invaders.shotX][shotY] === false) {
+                Invaders.shotX += 1;
+                if (Invaders.shotX > 10) {
+                    Invaders.shotX = 0;
+                    shotY += 1;
+                    if (shotY > 4) {
+                        shotY = 0;
+                    }
+                }
+            }
+            Invaders.alienShotX = Invaders.shotX * 48 + Invaders.alienX + 20;
+            Invaders.alienShotY = shotY * 48 + Invaders.alienY + 36;
+            Invaders.alienShot = true;
+        }
     }
     if (Invaders.alienShotY > 600) {
         Invaders.alienShot = false;
     }
     
-    if (Invaders.alienMotherShipCounter <= 0) {
+    if (Invaders.alienMotherShipCounter <= 0 && Invaders.alienMotherShipScoreCounter <= 0) {
         Invaders.alienMotherShipCounter = 500;
         Invaders.alienMotherShipOn = true;
         if (Math.random() > 0.5) {
@@ -301,7 +393,7 @@ function update() {
 
 function render() {
     "strict mode";
-    var x,y;
+    var x, y, row, col;
     
     Invaders.context.fillStyle = "black";
     Invaders.context.fillRect(0, 0, 800, 600);
@@ -327,7 +419,13 @@ function render() {
         Invaders.context.drawImage(Invaders.alienBullet, Invaders.alienShotX, Invaders.alienShotY);
     }
     for (x=0;x<6;x += 1) {
-        Invaders.context.drawImage(Invaders.playerBase, x * 110 + 101, 500);
+        for (col=0;col<3;col += 1) {
+            for (row=0;row<4;row += 1) {
+                if (Invaders.playerBaseGrid[x][row][col] !== 3) {
+                    Invaders.context.drawImage(Invaders.playerBase[Invaders.playerBaseGrid[x][row][col]], x * 110 + 101 + (col * 16), 516 + (row * 7));
+                }
+            }
+        }
     }
     
     Invaders.context.fillStyle = "white";
@@ -335,6 +433,9 @@ function render() {
     Invaders.context.fillText("Score: " + Invaders.score, 10, 20);
     Invaders.context.fillText("Level: " + Invaders.level, 400, 20);
     Invaders.context.fillText("Lives: " + Invaders.lives, 650, 20);
+    if (Invaders.alienMotherShipOn === false && Invaders.alienMotherShipScoreCounter > 0) {
+        Invaders.context.fillText("" + Invaders.alienMotherShipScore, Invaders.alienMotherShipX + 10, Invaders.alienMotherShipY + 40);
+    }
     
     if (Invaders.lives <= 0) {
         Invaders.context.fillText("GAME OVER!", 350, 300);
